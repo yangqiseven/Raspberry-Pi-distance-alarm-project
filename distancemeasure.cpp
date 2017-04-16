c2ff7578aa4294039ff2dd11f05ce6d09e382644
yangqiseven <yangqiseven@126.com>
2017-04-16 20:30:49 +0100

    Update distancemeasure.cpp

--- distancemeasure.cpp
+++ distancemeasure.cpp
@@ -2,31 +2,30 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 
-TRIG = 11
-ECHO = 12
+TRIG = 18
+ECHO = 16 #input pin is 11 and output pin is 9
 
-print "Distance Measurement In Progress"
 
 GPIO.setup(TRIC,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 
-GPIO.setup(TRIG, False)
-print "Waiting For Sensor To Settle"
-time.sleep(1)
+ while Ture:
+  GPIO.setup(TRIG, 0)
+  time.sleep(0.01)
 
-GPIO.output(TRIG, True)
-time.sleep(0.0001)
-GPIO.output(TRIG, False)
+  GPIO.output(TRIG, 1)
+  time.sleep(0.00001)
+  GPIO.output(TRIG, 0)
 
 while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
+  start = time.time()
   
 while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
+  stop = time.time()
   
-pules_duration = pulse_end - pulse_start
+duration = stop - start
 
-distance = pulse_duration * 17150
+distance = duration * (34000/2) #where the speed of sound is 340m/s
 
 distance = round(distance, 2)
 
