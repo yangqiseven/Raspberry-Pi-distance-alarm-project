0deb7d2c0b0a5efe89bad80daa35b4dd6d31a5dc
yangqiseven <yangqiseven@126.com>
2017-04-17 22:23:17 +0100

    Update distancemeasure-alarm.py

--- distancemeasure-alarm.py
+++ distancemeasure-alarm.py
@@ -1,41 +1,38 @@
 import RPi.GPIO as GPIO
 import time
+
+Trig_Pin = 20
+Echo_Pin = 21
+Led_Pin = 16
+maximum_dis = 80
+
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(Trig_Pin, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(Led_Pin, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(Echo_Pin, GPIO.IN)
 
-TRIG = 18
-ECHO = 16 #input pin is 11 and output pin is 9
+time.sleep(2)
 
+def checkdist():
+    GPIO.output(Trig_Pin, GPIO.HIGH)
+    time.sleep(0.00015)
+    GPIO.output(Trig_Pin, GPIO.LOW)
+    while not GPIO.input(Echo_Pin):
+        pass
+    t1 = time.time()
+    while GPIO.input(Echo_Pin):
+        pass
+    t2 = time.time()
+    
+    if ((t2-t1)*340*100/2) > maximum_dis:
+        GPIO.output(Led_Pin, 1)
+    if ((t2-t1)*340*100/2) < maximum_dis:     
+        GPIO.output(Led_Pin, 0)
+    return (t2-t1)*340*100/2
 
-GPIO.setup(TRIC,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
-
- while Ture:
-  GPIO.setup(TRIG, 0)
-  time.sleep(0.01)
-
-  GPIO.output(TRIG, 1)
-  time.sleep(0.00001)
-  GPIO.output(TRIG, 0)
-
-while GPIO.input(ECHO)==0:
-  start = time.time()
-  
-while GPIO.input(ECHO)==1:
-  stop = time.time()
-  
-duration = stop - start
-
-distance = duration * (34000/2) #where the speed of sound is 340m/s
-
-distance = round(distance, 2)
-
-print "Distance:", distance, "cm"
-
-minimum_alert_dis = 1
-maximum_alert_dis = 60
- if distance > 60
-         print "Distancesafe:",distance - 0.5, "cm"
-         if distance >= minimum_alert_dis and distance <= maximuma_alert_dis:
-                 print "Distance Alert!"
-
+try:
+    while True:
+        print 'Distance:%0.2f cm' % checkdist()
+        time.sleep(1)
+except KeyboardInterrupt:
 GPIO.cleanup()
